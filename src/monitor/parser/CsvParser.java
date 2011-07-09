ca682fe2b7f9a2f3862059dac2d27885fcbfa3e9
Jillian Kim <i2fifteen@Jillian-Kims-MacBook.local>
2011-07-09 16:36:26 -0700

    added csv parser file skeleton

---
+++ src/monitor/parser/CsvParser.java
@@ -0,0 +1,22 @@
+package monitor.parser;
+
+import java.io.*;
+import java.util.*;
+import monitor.*;
+
+public class CsvParser {
+ 
+    private Experiment experiment;
+    
+    
+    public void parseFile(File file) {
+        
+        experiment.setNumber(513);
+
+    }
+    
+    public Experiment getExperiment() {
+        return experiment;
+    }
+    
+}
