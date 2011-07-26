f03ec1da29aa35d95382c340f7b627a32dee5ca3
Brady Olsen <bradyo@uw.edu>
2011-07-25 17:32:48 -0700

    added to csv parser

--- src/monitor/parser/CsvParser.java
+++ src/monitor/parser/CsvParser.java
@@ -25,7 +25,7 @@ public class CsvParser {
         // save which columns have which row indexes in a hash map
         Map<String,Integer> headerIndexMap = new HashMap();
         for(Integer i = 0; i < values.length; i++) {
-            String columnName = values[i];
+            String columnName = values[i].toLowerCase();
             headerIndexMap.put(columnName, i);
         }
         
@@ -33,15 +33,20 @@ public class CsvParser {
         List<MotherCellSet> cellSets = new ArrayList();
         Integer row = 1; // start at 1, since we already read header line above
         while ((values = csvReader.readNext()) != null) {
-            // todo: create mother cell set
             MotherCellSet cellSet = new MotherCellSet();
-            cellSet.setId(row + 1);
-            cellSet.setReference(references[row]);
+            cellSet.setId(row);
+            
+            Integer referenceIndex = headerIndexMap.get("reference");
+            String reference = values[referenceIndex];
+            cellSet.setReference(reference);
+            
+            // todo: add other data to mother cell set
             cellSet.setLabel(labels[row]);
             cellSet.setStrainName(strainNames[row]);
             cellSet.setMedia(medias[row]);
             cellSet.setTemperature(temperatures[row]);
 
+            // add cells
             List<MotherCell> cells = new ArrayList();
             Integer[] rowLifespans = lifespans[rowIndex];
             for (Integer cellIndex = 0; cellIndex < rowLifespans.length; cellIndex++) {
@@ -50,6 +55,10 @@ public class CsvParser {
                 motherCell.setLifespan(rowLifespans[cellIndex]);
                 cells.add(motherCell);
             }
+            cellSet.setCells(cells);
+            
+            // save mother cell set internally
+            this.motherCellSets.add(cellSet);
             
             row++;
         }
